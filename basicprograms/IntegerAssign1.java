92f6e3b60806dadb3e9015cfc828970221f43ea0
Sandeep Jadhav <sandeep.jadhav@I2I.INTL>
2023-12-12 16:56:30 +0530

    initial

---
+++ basicprograms/IntegerAssign1.java
@@ -0,0 +1,14 @@
+package basicprograms;
+
+public class IntegerAssign1 {
+    public static void main(String[] args) {
+        int s = 19;
+        int r = 12;
+        int j = 15;
+        System.out.println(s+"Nineteen");
+        System.out.println(r+"Twelve");
+        System.out.println(j+"Fifteen");
+    }
+    
+    
+}
